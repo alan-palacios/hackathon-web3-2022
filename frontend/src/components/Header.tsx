049dfad24e31fee9fa5c29be1fea673cfaa82b0d
Zepeta <aggio_we@outlook.com>
2022-12-16 12:27:10 -0600

    update header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import Account from "./Account";
+import Button from "./Button";
 
 type Props = {
 	isAccountVisible: boolean;
@@ -6,7 +7,13 @@ type Props = {
 
 export default function Header({ isAccountVisible }: Props) {
 	return (
-		<header className="flex justify-end pr-5">
+		<header className="flex justify-between pr-5 items-center">
+			<div className="ml-5 mt-2">
+				<a href="https://github.com/alan-palacios/hackathon-web3-2022/tree/main/domain-template"
+					target="_blank" rel="noreferrer">
+					<Button label="VIEW CONTRACT TEMPLATE" />
+				</a>
+			</div>
 			{isAccountVisible && <Account />}
 		</header>
 	);
