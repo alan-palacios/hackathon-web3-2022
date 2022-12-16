023245e1d50f8f781bee4af475cc0bbf3a2a5245
alan-palacios <apyolt@gmail.com>
2022-12-15 18:09:31 -0600

    add dns metadata

--- contract/io/src/lib.rs
+++ contract/io/src/lib.rs
@@ -16,6 +16,10 @@ pub struct DnsMeta {
     pub name: String,
     pub link: String,
     pub description: String,
+    //pub category: String,
+    //pub tags: String[],
+    //pub date: String,
+    //pub image: String,
 }
 
 #[derive(Decode, Clone)]
