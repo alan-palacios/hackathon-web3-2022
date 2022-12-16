8ef3100ac6a17e5a8802be1b82dec7e5c9084653
Zepeta <aggio_we@outlook.com>
2022-12-16 09:18:48 -0600

    add icions

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,73 +2,76 @@ import { Button } from "@gear-js/ui";
 import { bufferString } from "assets/metaBuffer";
 import Input from "components/Input";
 import { Select } from "components/Select";
-import { connect } from "hooks/useConnection"
 import { useReadConfig } from "hooks/useReadConfig";
 import { useEffect, useMemo, useState } from "react";
+import { Icon } from '@iconify/react';
 
 export default function Home() {
 	const [selectValue, setSelectValue] = useState('GetAll');
 	const [search, setSearch] = useState('');
 	const [programId, setProgramId] = useState('');
-	// console.log(connect);
-  const metaBuffer = useMemo(() => Buffer.from(bufferString, 'base64'), []);
+	const metaBuffer = useMemo(() => Buffer.from(bufferString, 'base64'), []);
 	const { stateAll } = useReadConfig(metaBuffer, selectValue, search);
-	console.log(stateAll);
-	
-  useEffect(() => {
-    if (stateAll.error) {
-      setSelectValue('GetAll');
-      setSearch('');
-    }
-  }, [stateAll.error]);
-	
+	// console.log(stateAll);
+
+	useEffect(() => {
+		if (stateAll.error) {
+			setSelectValue('GetAll');
+			setSearch('');
+		}
+	}, [stateAll.error]);
+
 	const readDataFrom = (urlLink: string | undefined) => {
-    if (!urlLink) return;
-    window.open(urlLink);
-  };
-	
+		if (!urlLink) return;
+		window.open(urlLink);
+	};
+
 	const getElementValue = useMemo(
-    () =>
-      stateAll.state?.Records.length ? (
-        stateAll.state?.Records.map((element) => (
-          <li key={element.id}
-						className='w-80 h-80 bg-neutral-800 rounded-lg flex flex-col justify-center'>
-            <span>{element.meta.name}</span>
-            <span>{element.id}</span>
-            <span>{element.meta.description}</span>
-            <span>{element.createdBy}</span>
-            <span>{element.meta.link}</span>
-            <Button type="button" text="Open" size="small" onClick={() => readDataFrom(element.meta.link)} />
-          </li>
-        ))
-      ) : (
-        <h4>Results wasnt found</h4>
-      ),
-    [stateAll.state?.Records],
-  );
+		() =>
+			stateAll.state?.Records.length ? (
+				stateAll.state?.Records.map((element) => (
+					<li key={element.id}
+						className='w-full bg-neutral-800 rounded-lg flex flex-col justify-center'>
+						<span>{element.meta.name}</span>
+						<span>{element.id}</span>
+						<span>{element.meta.description}</span>
+						<span>{element.createdBy}</span>
+						<span>{element.meta.link}</span>
+						<Button type="button" text="Open" size="small" onClick={() => readDataFrom(element.meta.link)} />
+					</li>
+				))
+			) : (
+				<h4>Results wasnt found</h4>
+			),
+		[stateAll.state?.Records],
+	);
 
 	return (
 		<div className="flex flex-col justify-center bg-gradient-to-b from-dark1 to-dark2 min-h-full h-full">
-      <div className="flex flex-col items-center">
-        <div>
-          The chain or icon here
-        </div>
-        <div>
-          Where blocks connect... or some phrase here
-        </div>
-        <Input value={search} onChange={setSearch} />
-      </div>
-      {stateAll.isStateRead ? (
-        <div>
-          {getElementValue}
-        </div>
-      ) : (
-        <span>Loading ...</span>
-      )}
-			<div className="bg-neutral-700">
-				<Select handleSelectedValue={setSelectValue} />
-				<Input value={search} onChange={setSearch} />
+			<div className="flex flex-col items-center px-2">
+				<div>
+					The chain or icon here
+				</div>
+				<div>
+					Where blocks connect... or some phrase here
+				</div>
+				{/* <Input value={search} onChange={setSearch} /> */}
 			</div>
+			<div className="bg-neutral-700 w-full flex flex-col justify-center items-center">
+				<div className="flex">
+					<Input value={search} onChange={setSearch} />
+					<Icon icon="mdi-light:search" />
+				</div>
+
+				<Select handleSelectedValue={setSelectValue} />
+			</div>
+			{stateAll.isStateRead ? (
+				<div>
+					{getElementValue}
+				</div>
+			) : (
+				<span>Loading ...</span>
+			)}
 		</div>
 	)
 }
